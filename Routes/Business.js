f5a9ba58162ae86b7fc8f69e438a7b8f07ff4141
Gentiana 21 <99845020+Gentiana20@users.noreply.github.com>
2024-06-14 00:21:12 +0200

    Create Business.js

---
+++ Routes/Business.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const businessController = require('../Controllers/Business');
+
+// GET all businesses
+router.get('/', businessController.getAllBusinesses);
+
+// GET single business
+router.get('/:id', businessController.getSingleBusiness);
+
+// POST create new business
+router.post('/', businessController.createBusiness);
+
+// DELETE a business
+router.delete('/:id', businessController.deleteBusiness);
+
+// PUT update a business
+router.patch('/:id', businessController.updateBusiness);
+
+module.exports = router;
