d80cec3a5fe3d344b42fb2fa7ac3ff961e6e07eb
Gentiana 21 <99845020+Gentiana20@users.noreply.github.com>
2024-06-14 00:19:28 +0200

    Create questionType.js

---
+++ Models/questionType.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+const questionTypeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('QuestionType', questionTypeSchema);
+
