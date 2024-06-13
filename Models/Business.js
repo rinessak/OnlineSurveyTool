cdd9e0324586657eaffd6759e98fe97c6fab558d
Gentiana 21 <99845020+Gentiana20@users.noreply.github.com>
2024-06-14 00:18:58 +0200

    Create Business.js

---
+++ Models/Business.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const businessSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Business', businessSchema);
