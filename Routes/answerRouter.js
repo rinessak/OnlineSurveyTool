a2c76ff7ff99f45626cff42d1d2b99005a206512
rinessak <102384321+rinessak@users.noreply.github.com>
2024-06-14 10:17:52 +0200

    Update answerRouter.js

--- Routes/answerRouter.js
+++ Routes/answerRouter.js
@@ -1,13 +1,11 @@
-// routes/answersRoutes.js
 const express = require('express');
+const answerController = require('../Controllers/answerController');
 const router = express.Router();
-const answersController = require('../Controllers/answersController');
 
-// Routes
-router.get('/', answersController.getAllAnswers);
-router.get('/question/:questionId', answersController.getAnswersByQuestionId);
-router.post('/', answersController.createAnswer);
-// router.put('/:answerId', answersController.updateAnswer);
-
+router.get('/', answerController.getAllAnswers);
+router.get('/:id', answerController.getSingleAnswer);
+router.post('/', answerController.createAnswer);
+router.patch('/:id', answerController.updateAnswer);
+router.delete('/:id', answerController.deleteAnswer);
 
 module.exports = router;
