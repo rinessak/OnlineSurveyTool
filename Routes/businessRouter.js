e786a7435e6d3ed5f08a473eabec9f0eb078a658
Gentiana Rama <99845020+Gentiana20@users.noreply.github.com>
2024-07-10 22:46:01 +0200

    Update businessRouter.js

--- Routes/businessRouter.js
+++ Routes/businessRouter.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const businessController = require('../Controllers/businessController');
+const businessController = require('../Controllers/businessController'); // Corrected path to controller
 
 // GET all businesses
 router.get('/', businessController.getAllBusinesses);
 
-// GET single business
-router.get('/:id', businessController.getSingleBusiness);
+// GET single business by ID
+router.get('/:id', businessController.getBusinessById);
 
 // POST create new business
 router.post('/', businessController.createBusiness);
 
-// DELETE a business
+// DELETE a business by ID
 router.delete('/:id', businessController.deleteBusiness);
 
-// PUT update a business
+// PUT update a business by ID
 router.patch('/:id', businessController.updateBusiness);
 
 module.exports = router;
