2cc0f54639668aa96e139ec17154738b3ce079e6
Gentiana Rama <99845020+Gentiana20@users.noreply.github.com>
2024-07-10 22:44:09 +0200

    Update business.js

--- Models/business.js
+++ Models/business.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const businessSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
+    fiscalNumber: { type: String, required: true },
+    address: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
 });
