4c53d0b676d46e4d60a2b2888f0021f2d394c340
rinessak <102384321+rinessak@users.noreply.github.com>
2024-07-11 12:00:25 +0200

    Update business.js

--- Models/business.js
+++ Models/business.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const businessSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    fiscalNumber: { type: String, required: true },
+    fiscalNumber: {
+        type: String,
+        required: true,
+        unique: true},
     address: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
