b46bf2849c1a061d8cc805efc527e834bfb022c3
rinessak <102384321+rinessak@users.noreply.github.com>
2024-07-12 12:22:58 +0200

    Update answerController.js

--- Controllers/answerController.js
+++ Controllers/answerController.js
@@ -1,67 +1,125 @@
-const Answers = require("../Models/answers");
+const Answer = require('../Models/answers');
+const Question = require('../Models/question');
+const Survey = require('../Models/survey')
 
-const getAllAnswers = async (req, res) => {
+// Get all answers
+exports.getAllAnswers = async (req, res) => {
     try {
-        const answers = await Answers.find();
-        res.status(200).json(answers);
+        const answers = await Answer.find();
+        res.json(answers);
     } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-const getSingleAnswer = async (req, res) => {
-    const id = req.params.id;
-    try {
-        const answer = await Answers.findOne({ _id: id });
-        res.status(200).json(answer);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-const createAnswer = async (req, res) => {
-    const { question, survey, responder, answer } = req.body;
-
-    // Check for required fields
-    if (!question || !survey || !answer) {
-        return res.status(400).json({ message: "Required fields are missing" });
-    }
-
-    try {
-        // Create a new answer object with the provided data
-        const newAnswer = await Answers.create({
-            question,
-            survey,
-            responder,
-            answer,
-        });
-
-        res.status(201).json(newAnswer);
-
-    } catch (error) {
-        // Handle internal server errors
-        res.status(500).json({ message: error.message });
+        console.error(error);
+        res.status(500).send('Error retrieving answers');
     }
 };
 
-const updateAnswer = async (req, res) => {
-    const id = req.params.id;
+// Get a single answer by ID
+exports.getSingleAnswer = async (req, res) => {
     try {
-        const updatedAnswer = await Answers.findOneAndUpdate({ _id: id }, req.body, { new: true });
-        res.status(200).json(updatedAnswer);
+        const answer = await Answer.findById(req.params.id);
+        if (!answer) {
+            return res.status(404).send('Answer not found');
+        }
+        res.json(answer);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.error(error);
+        res.status(500).send('Error retrieving answer');
     }
-}
+};
 
-const deleteAnswer = async (req, res) => {
-    const id = req.params.id;
+// Create a new answer
+exports.createAnswer = async (req, res) => {
+    const { questionId, surveyId, responderId, answer, otherAnswer } = req.body;
+
     try {
-        await Answers.findOneAndDelete({ _id: id });
-        res.status(204).json({ message: "Answer deleted successfully" });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+        if (!questionId || !surveyId) {
+            return res.status(400).send('QuestionId and SurveyId are required');
+        }
 
-module.exports = { getAllAnswers, getSingleAnswer, createAnswer, updateAnswer, deleteAnswer };
+        const question = await Question.findById(questionId);
+        if (!question) {
+            return res.status(404).send('Question not found');
+        }
+
+        const survey = await Survey.findById(surveyId);
+        if (!survey) {
+            return res.status(404).send('Survey not found');
+        }
+
+        const newAnswer = new Answer({
+            questionId: questionId,
+            surveyId: surveyId,
+            responderId: responderId,
+            answer: answer,
+            otherAnswer: otherAnswer
+        });
+
+        await newAnswer.save();
+
+        res.status(200).json(newAnswer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error submitting answer');
+    }
+};
+
+
+exports.updateAnswer = async (req, res) => {
+    const { answer, otherAnswer } = req.body;
+    try {
+        let updatedAnswer = await Answer.findByIdAndUpdate(
+            req.params.id,
+            { answer, otherAnswer },
+            { new: true }
+        );
+        if (!updatedAnswer) {
+            return res.status(404).send('Answer not found');
+        }
+        res.json(updatedAnswer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating answer');
+    }
+};
+
+// Delete an answer
+exports.deleteAnswer = async (req, res) => {
+    try {
+        const answer = await Answer.findByIdAndDelete(req.params.id);
+        if (!answer) {
+            return res.status(404).send('Answer not found');
+        }
+        res.json({ msg: 'Answer deleted' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting answer');
+    }
+};
+
+// Submit answer 
+exports.submitAnswer = async (req, res) => {
+    const { questionId, surveyId, responderId, answer, otherAnswer } = req.body;
+
+    try {
+        const question = await Question.findById(questionId);
+
+        if (!question) {
+            return res.status(404).send('Question not found');
+        }
+
+        const newAnswer = new Answer({
+            questionId: questionId,
+            surveyId: surveyId,
+            responderId: responderId,
+            answer: answer,
+            otherAnswer: otherAnswer
+        });
+
+        await newAnswer.save();
+        res.status(200).send('Answer submitted successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error submitting answer');
+    }
+};
+
